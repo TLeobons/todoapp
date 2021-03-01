fb6e552aa1cf67f5e165736015e48db9b06678cb
Ezekiel <zico4reel@gmail.com>
2021-03-01 15:11:22 +0100

    Fixing the errors, first thing, ensure your break in switch is okay, next the reason for undefined is because of the remove button

--- src/App - Copy.js
+++ src/App - Copy.js
@@ -1,9 +1,10 @@
-import { useState, useReducer, useMemo } from "react"
+import { useState, useReducer } from "react"
 import {v4} from "uuid"
 
 import {ADD_TODO, REMOVE_TODO} from './types'
 import './App.css'
 
+function App() {
 
   const task = {
     id: '',
@@ -12,12 +13,13 @@ import './App.css'
     collection: ''
   }
 
+  const [todo, setTodo] = useState('')
 
   const onlyReducer = (state, action) => {
-    //console.log('state', state)
+    console.log('state', state)
     switch (action.type){
       case ADD_TODO:
-		return [
+        return [
           ...state,
           {
             ...task,
@@ -25,43 +27,33 @@ import './App.css'
             name: action.payload
           }
         ]
-	break;
         case REMOVE_TODO:
           // state.map()
-          return state.filter(item=>item.id!==action.payload);
-	break;
+          return {
+          }
       default :
         return state
     }
   }
 
-
-
-function App() {
-
-	const [todo, setTodo] = useState('');
   const [state, dispatch] = useReducer(onlyReducer, [])
 
-  const handleSubmit = e =>{
-    	e.preventDefault()
- 	dispatch({type: 'ADD_TODO', payload: todo})
-	setTodo('');
-  };
-	//log state, only when it has changed
-	useMemo(()=>console.log('state',state),[state]);
+  const handleSubmit = e => {
+    e.preventDefault()
+    dispatch({type: 'ADD_TODO', payload: todo})
+  }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input
         value={todo}
-        onChange={e=>setTodo(e.target.value)}
-	/>
+        onChange={e=>setTodo(e.target.value)}/>
 
-        {state.map(task => (
+        {(state || []).map(task => (
           <div key={task.id}>
             <span>{task.name}</span>
-            <button type="button" onClick={() => dispatch({type: 'REMOVE_TODO', payload: task.id})}>X</button>
+            <button onClick={() => dispatch({type: 'REMOVE_TODO', payload: task.id})}>X</button>
           </div>
         ))}
       </form>
