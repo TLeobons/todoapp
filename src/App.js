b9b4e54df35a1e1ca7abfb82359c90c156bbc0c2
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2021-03-01 19:06:34 +0530

    Delete function added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react"
+import {useState, useReducer} from "react"
 import {v4} from "uuid"
 
 import {ADD_TODO, REMOVE_TODO} from './types'
@@ -6,62 +6,63 @@ import './App.css'
 
 function App() {
 
-  const task = {
-    id: '',
-    name: '',
-    done: false,
-    collection: ''
-  }
-
-  const [todo, setTodo] = useState('')
-
-  const onlyReducer = (state, action) => {
-    switch (action.type){
-      case ADD_TODO:
-        return [
-          ...state,
-          {
-            ...task,
-            id: v4(),
-            name: action.payload
-          }
-        ]
-      case REMOVE_TODO:
-        return { state.filter(item => item.id !== payload)}
-      default :
-        return new Error(`Invalid action ${action.type}`)
-      }
+    const task = {
+        id: '',
+        name: '',
+        done: false,
+        collection: ''
     }
-      
-  const [state, dispatch] = useReducer(onlyReducer, [])
-  
-  const handleSubmit = e => {
-    e.preventDefault()
-    dispatch({type: 'ADD_TODO', payload: todo})
-    setTodo('')
-  }
-  
-  return (
-    <div className="App">
-    {console.log('state', state)}
-      <form onSubmit={handleSubmit}>
-        <input
-        value={todo}
-        onChange={e=>setTodo(e.target.value)}/>
 
-        {state.map(task => (
-          <div key={task.id}>
-            <span>{task.name}</span>
-            <button
-              type='button'
-              onClick={() => dispatch({type: 'REMOVE_TODO', payload: task.id})}>
-              X
-            </button>
-          </div>
-        ))}
-      </form>
-    </div>
-  );
+    const [todo, setTodo] = useState('')
+
+    const onlyReducer = (state, action) => {
+        switch (action.type) {
+            case ADD_TODO:
+                return [
+                    ...state,
+                    {
+                        ...task,
+                        id: v4(),
+                        name: action.payload
+                    }
+                ]
+            case REMOVE_TODO:
+                let filterArray = state.filter(item => item.id !== action.payload)
+                return [...filterArray]
+            default :
+                return new Error(`Invalid action ${action.type}`)
+        }
+    }
+
+    const [state, dispatch] = useReducer(onlyReducer, [])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        dispatch({type: 'ADD_TODO', payload: todo})
+        setTodo('')
+    }
+
+    return (
+        <div className="App">
+            {console.log('state', state)}
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={todo}
+                    onChange={e => setTodo(e.target.value)}/>
+
+                {state.map(task => (
+                    <div key={task.id}>
+                        <span>{task.name}</span>
+                        <button
+                            type='button'
+                            onClick={() => dispatch({type: 'REMOVE_TODO', payload: task.id})}>
+                            X
+                        </button>
+                    </div>
+                ))}
+            </form>
+        </div>
+    );
 }
 
 export default App
